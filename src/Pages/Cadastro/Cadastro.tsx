a0be4127102be85fa1e225344df4a81ad5cb8d0d
Joao <joaomatheusserafim@gmail.com>
2026-01-30 08:53:55 -0300

    novo

--- src/Pages/Cadastro/Cadastro.tsx
+++ src/Pages/Cadastro/Cadastro.tsx
@@ -238,7 +238,6 @@ export default function CadastroAtleta() {
       setLoadingExport(false);
     }
   };
-
   return (
     <div className="min-h-screen bg-gray-50 py-6 px-4">
       <div className="max-w-7xl mx-auto">
